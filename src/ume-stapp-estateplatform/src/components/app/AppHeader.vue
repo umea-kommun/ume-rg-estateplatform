cae9bac1f03896932dd8bba1baa56dc90ad5773a
Simon Andreas Clausén <simonandreas.clausen@umea.se>
2025-08-20 09:47:08 +0000

    Merged PR 2377: Errorbanner: "Mycket trafik..."
    Errorbanner: "Mycket trafik..."

--- src/ume-stapp-estateplatform/src/components/app/AppHeader.vue
+++ src/ume-stapp-estateplatform/src/components/app/AppHeader.vue
@@ -1,5 +1,8 @@
 <template>
 	<header class="app-header">
+		<v-alert class="ma-4" icon="info" color="info">
+			{{ $t('app.errorbanner') }}
+		</v-alert>
 		<div class="mock-alert-wrap" v-if="usingMockedData">
 			<div class="mock-alert">
 				<div>
